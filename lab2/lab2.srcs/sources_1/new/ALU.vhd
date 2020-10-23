5642e39f2868cbdf1b45f1cae09ec9cc5b2a8402
Miguel Reis <miguelareis98@gmail.com>
2020-10-23 15:13:31 +0100

    Added ALU and Diagram

---
+++ lab2/lab2.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_SIGNED.all;
+
+entity ALU is
+    Port ( A : in STD_LOGIC_VECTOR (16 downto 0);
+           B : in STD_LOGIC_VECTOR (16 downto 0);
+           oper : in STD_LOGIC;
+           result : out STD_LOGIC_VECTOR (16 downto 0));
+end ALU;
+
+architecture Behavioral of ALU is
+begin
+    PROCESS (A,B,oper)
+        BEGIN
+        IF oper = '0' then
+            result <= A+B;
+        ELSE
+            result <= A-B;
+        END IF;
+    END PROCESS;
+end Behavioral;
